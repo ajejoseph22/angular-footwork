b82dbdc570e400cb76f7abb5ecff1128e976970a
Joseph Aje <ajejoseph22@gmail.com>
2019-11-24 12:10:01 +0100

    added spinner to product-list view

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,16 +1,36 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import ProductService from './products.service';
+import {IProduct} from './product';
 
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  providers: [ProductService]
 })
 export class ProductDetailComponent implements OnInit {
+  private product: IProduct;
+  private errorMessage: string;
 
-  constructor() {
+  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) {
+  }
+
+  isLoading = true;
+
+  onBack(): void {
+    this.router.navigate(['products']);
   }
 
   ngOnInit() {
-  }
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.product = products.find(product => product.productId === id);
+        this.isLoading = false;
 
+      },
+      error: err => (this.errorMessage = err),
+    });
+  }
 }
